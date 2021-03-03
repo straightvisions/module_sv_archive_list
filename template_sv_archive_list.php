08029bf053b4f3f5274590283a5462f9aed94f7e
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-03-03 23:08:42 +0100

    ADD ARCHIVE TEMPLATE LANGUAGE SUPPORT

--- template_sv_archive_list.php
+++ template_sv_archive_list.php
@@ -4,6 +4,8 @@
 		// complete template logic should be here
 		class template_sv_archive_list extends \sv_core\abstract_template_sv_archive{
 			protected function load_settings(): \sv_core\abstract_template_sv_archive{
+				load_theme_textdomain( 'template_sv_archive_list', $this->get_path( 'languages' ) );
+
 				$this->load_settings_common()
 				->load_settings_entry()
 				->load_settings_parts()
