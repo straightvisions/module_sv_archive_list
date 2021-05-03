dd635345a62e2505d4e4bf120fa208c11c208361
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-05-03 15:32:01 +0200

    FIX - DESIGN

--- lib/tpl/frontend/parts/featured_image.php
+++ lib/tpl/frontend/parts/featured_image.php
@@ -1,1 +1,1 @@
-<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+<?php if(has_post_thumbnail()){ ?><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a><?php }
--- lib/tpl/frontend/parts/header.php
+++ lib/tpl/frontend/parts/header.php
@@ -1,4 +1,4 @@
-<header class="page-header alignwide">
+<header class="page-header">
 	<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
 	<?php if ( get_the_archive_description() ) { ?>
 		<div class="archive-description"><?php echo wp_kses_post( wpautop( get_the_archive_description() ) ); ?></div>
