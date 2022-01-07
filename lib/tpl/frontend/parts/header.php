55401b628d6527fd610fcfac143f48c959e1d349
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-01-07 17:45:55 +0100

    FIX ARCHIVE TITLE FOR SEARCH

--- lib/tpl/frontend/parts/header.php
+++ lib/tpl/frontend/parts/header.php
@@ -1,5 +1,11 @@
 <header class="page-header">
-	<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+	<?php
+		if(is_search()){
+			echo '<h1 class="page-title">'; printf( esc_html__( 'Search Results for: %s', 'sv100' ), '<span>' . get_search_query() . '</span>' ); echo '</h1>';
+		}else {
+			the_archive_title('<h1 class="page-title">', '</h1>');
+		}
+		?>
 	<?php if ( get_the_archive_description() ) { ?>
 		<div class="archive-description"><?php echo wp_kses_post( wpautop( get_the_archive_description() ) ); ?></div>
 	<?php } ?>
